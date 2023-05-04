29448cde9af3ac41555fa34a7e329108be8a478f
Lucas Gabriel Palamedi <30753568+LucasPalamedi@users.noreply.github.com>
2023-05-04 09:11:53 -0300

    add files via upload
    add files via upload

---
+++ app/main.js
@@ -0,0 +1,9 @@
+let livros = []
+getBuscarLivrosDaAPI()
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
